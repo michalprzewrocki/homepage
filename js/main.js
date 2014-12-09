7329f68e75f2acef17e6b736f63fddb2dc60ae51
michal.przewrocki@gmail.com <michal.przewrocki@gmail.com>
2014-12-10 00:24:22 +0100

    [DEV] loading

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 
+$(document).ready(function(){
+	set_loader();
+});
+
+$(window).resize(function(){
+	set_loader();
+});
+
+function set_loader(){
+	var height = $(window).height();
+	var width = $(window).width();
+	center_loader(height,width);
+};
+
+
+function center_loader(h,w){
+	var margin_top = Math.floor(h/2 - $('#loader-big-content').height()/2);
+	console.log(margin_top);
+	$('#loader-big-content').css('top', margin_top);
+}
